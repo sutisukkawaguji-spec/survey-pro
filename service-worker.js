d9dde6f3109597724a894e3c5b42951bc0403c14
sutisukkawaguji-spec <sutisuk.kawaguji@gmail.com>
2026-02-19 12:16:59 +0700

    Update cache in service worker to include icon.png
    Removed external Font Awesome and Leaflet resources from cache and added local icon.png.

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,11 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+  './icon.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
